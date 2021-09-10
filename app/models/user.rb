0ac71dd6a001da63e82dd8baf0611fbba8036e86
HijiriOkuhara <hidiri.s.b@gmail.com>
2021-09-10 21:51:01 +0900

    ビューファイルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :nickname, presence: true
-  validates :encrypted_password, format: { with: /[a-z\d]{6}/i}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6}/i}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   validates :last_name_furigana, presence: true, format: { with: /\A[ァ-ヶー]+\z/}
