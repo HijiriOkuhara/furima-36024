1d410775df4119f64cb6a7e9d63a67a5bffccc91
HijiriOkuhara <hidiri.s.b@gmail.com>
2021-09-11 19:04:27 +0900

    バリデーションを設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true
+  validates :state_id, presence: true
+  validates :delivery_charge_burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_day_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
 end
