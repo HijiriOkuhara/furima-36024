385861f9c1f8b8e74d786c5a6c144bdd157de059
HijiriOkuhara <hidiri.s.b@gmail.com>
2021-09-13 15:35:55 +0900

    Productモデルの単体テスト

---
+++ spec/factories/product.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :product do
+    name {"name"}
+    description {"description"}
+    category_id {2}
+    state_id {2}
+    delivery_charge_burden_id {2}
+    prefecture_id {2}
+    delivery_day_id {2}
+    price {500}
+    association :user
+    after(:build) do |product|
+      product.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
+  end
+end
