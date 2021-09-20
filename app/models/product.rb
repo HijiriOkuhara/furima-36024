f26e1eff64dbbf3778f6c0333a8fb39000784ec0
HijiriOkuhara <hidiri.s.b@gmail.com>
2021-09-20 20:51:47 +0900

    UserモデルとProductモデルのバリデーションをリファクタリング

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,14 +8,19 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one :purchase_record
-  validates :image, presence: true
-  validates :name, presence: true, length: { maximum: 40 }
-  validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_charge_burden_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, inclusion: { in: 300..9_999_999, message: 'is out of setting range' },
-                    numericality: { message: 'is invalid. Input half-width characters' }
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :price, inclusion: { in: 300..9_999_999, message: 'is out of setting range' },
+                      numericality: { message: 'is invalid. Input half-width characters' }
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :state_id
+      validates :delivery_charge_burden_id
+      validates :prefecture_id
+      validates :delivery_day_id
+    end
+  end
 end
